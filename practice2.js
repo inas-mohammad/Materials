9c2c25f468a2cd06c9972cb34dea1441f71afaed
inas-mohammad <inas.hameidan@gmail.com>
2021-01-21 22:28:55 +0200

    week3

---
+++ practice2.js
@@ -0,0 +1,155 @@
+console.log(iteration);
+console.log(Pulse Check);
+console.log(q1);
+while loop
+for loop
+
+console.log(q2);
+let i=0
+while(i<4){
+    console.log(i++);
+    i++;
+
+}
+console.log(q3);
+for (i=10;i<19;i++){
+    console.log(i++);
+    i++;
+
+}
+console.log(q4);
+const object = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+  };
+for (key in object){
+    console.log(key.object[key];}
+
+
+console.log(Practice);
+console.log(q1);
+const sum = function (numbers) {
+     let result=0;
+    for (i=0;i<number.length;i++){
+        resutl=result+number[i];
+        console.log(result);
+    }
+
+  };
+
+
+  console.log(q2);
+  const filter = function (numbers) {
+     const result=[];
+     for (i=0;i<number.length;i++){
+         if (number[i]%2 != 0 ){
+             result.push(number[i])
+      return result;
+            }
+     }
+  };
+
+  console.log(q3);
+  const maximumNumber = function (numbers) {
+  for (i=0;i<numbers.length;i++){
+      if (numbers[i]<numbers[i+1]){
+          return (numbers[i+1])
+      }
+      else{
+          return (number[i])
+      }
+  }
+}
+
+
+
+
+console.log(q4);
+const bills = {
+    waterBill: 25,
+    electricityBill: 50,
+    hospitalBill: 1000,
+  };
+  
+  const totalBill = function (billsObject) {
+    sum of value for each key.bills
+  };
+
+
+  console.log (q5);
+
+
+
+
+
+  console.log (Extra Practice)
+  console.log (q1);
+  const onlyEven = function (numbers) {
+    for (i=0;i<numbers.length;i++){
+     numbers.splice  (numbers[i]%2=0)      }
+        return(numbers[i])
+    }
+  };
+
+
+
+  console.log(q2);
+  const sort = function (numbers) {
+    
+  
+  };
+
+  console.log(q3);
+
+
+
+  console.log(q4);
+
+
+  console.log(q5);
+
+
+  console.log(q6);
+
+
+  console.log(pulse check);
+  console.log (q1);
+  Recursion is made for solving problems that can be broken down into smaller,
+   repetitive problems. It is especially good for working on things that have many possible
+    branches and are too complex for an iterative approach. One good example of this would be
+     searching through a file system.
+
+
+
+     console.log(q2);
+     to stop loop 
+
+     console.log(q3);
+     number - 1
+     
+     console.log(q4);
+
+     console.log(q5);
+     const countDown = function (number) {
+        console.log(number);
+        if (number === 0) {
+          return;
+        }
+        return (countDown(number - 1)+"-");
+      };
+
+
+
+      console.log(practice);
+      console.log(q1);
+      const sumCubes = function (number) {
+        // TODO: Your code here
+      };
+      
+  
+
+  
+
+
